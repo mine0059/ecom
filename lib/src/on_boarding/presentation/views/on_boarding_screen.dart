24be6769e8b16ab38a76e130628b4c76fc74c68b
ommanuel <eonojefemue@bluetag.tech>
2025-09-15 20:15:05 +0100

    redesign screen

--- lib/src/on_boarding/presentation/views/on_boarding_screen.dart
+++ lib/src/on_boarding/presentation/views/on_boarding_screen.dart
@@ -1,5 +1,45 @@
-import 'package:ecom/src/on_boarding/presentation/on_boarding_info_section.dart';
+// class OnBoardingScreen extends StatefulWidget {
+//   const OnBoardingScreen({super.key});
+
+//   @override
+//   State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+// }
+
+// class _OnBoardingScreenState extends State<OnBoardingScreen> {
+//   final pageController = PageController();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//           child: Padding(
+//         padding: const EdgeInsets.all(16),
+//         child: PageView(
+//           allowImplicitScrolling: true,
+//           controller: pageController,
+//           children: const [
+//             OnBoardingInfoSection.first(),
+//             OnBoardingInfoSection.second(),
+//           ],
+//         ),
+//       )),
+//     );
+//   }
+// }
+
+import 'package:ecom/core/utils/device/device_utility.dart';
+import 'package:ecom/core/utils/helpers/helper_functions.dart';
+import 'package:ecom/src/auth/presentation/views/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+import '../../../../core/common/app/cache_helper.dart';
+import '../../../../core/common/constants/colors.dart';
+import '../../../../core/common/constants/image_strings.dart';
+import '../../../../core/common/constants/sizes.dart';
+import '../../../../core/common/constants/text_strings.dart';
+import '../../../../core/services/injection_container.dart';
+import '../widgets/on_boarding_info.dart';
 
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({super.key});
@@ -9,22 +49,110 @@ class OnBoardingScreen extends StatefulWidget {
 }
 
 class _OnBoardingScreenState extends State<OnBoardingScreen> {
-  final pageController = PageController();
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  void _onPageChange(int page) {
+    setState(() {
+      _currentPage = page;
+    });
+  }
+
+  void _nextPage() {
+    if (_currentPage < 2) {
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    } else {
+      // handle completion of onboarding
+      sl<CacheHelper>().cacheFirstTimer();
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    }
+  }
+
+  void _skipOnboarding() {
+    sl<CacheHelper>().cacheFirstTimer();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginScreen()),
+    );
+    // context.go(LoginScreen.path);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: PageView(
-          allowImplicitScrolling: true,
-          controller: pageController,
+        body: Stack(
+      children: [
+        PageView(
+          controller: _pageController,
+          onPageChanged: _onPageChange,
           children: const [
-            OnBoardingInfoSection.first(),
-            OnBoardingInfoSection.second(),
+            OnBoardingPageInfos(
+              image: TImages.onBoardingImage1,
+              title: TTexts.onBoardingTitle1,
+              subTitle: TTexts.onBoardingSubTitle1,
+            ),
+            OnBoardingPageInfos(
+              image: TImages.onBoardingImage2,
+              title: TTexts.onBoardingTitle2,
+              subTitle: TTexts.onBoardingSubTitle2,
+            ),
+            OnBoardingPageInfos(
+              image: TImages.onBoardingImage3,
+              title: TTexts.onBoardingTitle3,
+              subTitle: TTexts.onBoardingSubTitle3,
+            ),
           ],
         ),
-      )),
+        _buildSkipButton(),
+        _buildDot(context),
+        _buildCircularButton(context),
+      ],
+    ));
+  }
+
+  Widget _buildSkipButton() {
+    return Positioned(
+      top: TDeviceUtils.getAppBarHeight(),
+      right: TSizes.defaultSpace,
+      child: TextButton(onPressed: _skipOnboarding, child: const Text('Skip')),
+    );
+  }
+
+  Widget _buildDot(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Positioned(
+      bottom: TDeviceUtils.getBottomNavigationBarHeight() + 25,
+      left: TSizes.defaultSpace,
+      child: SmoothPageIndicator(
+        controller: _pageController,
+        onDotClicked: _onPageChange,
+        count: 3,
+        effect: ExpandingDotsEffect(
+          activeDotColor: dark ? TColors.light : TColors.dark,
+          dotHeight: 6,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCircularButton(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Positioned(
+      right: TSizes.defaultSpace,
+      bottom: TDeviceUtils.getBottomNavigationBarHeight(),
+      child: ElevatedButton(
+        onPressed: _nextPage,
+        style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(),
+            backgroundColor: dark ? TColors.primary : Colors.black),
+        child: const Icon(Iconsax.arrow_right_3),
+      ),
     );
   }
 }
