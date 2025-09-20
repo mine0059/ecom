8f6359eaca4e4eea52ce66df15a3da5fd9a98199
ommanuel <eonojefemue@bluetag.tech>
2025-09-20 19:27:03 +0100

    reusable shadow widget for product card

---
+++ lib/core/common/styles/shadows.dart
@@ -0,0 +1,18 @@
+import 'package:ecom/core/common/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class ShadowsStyle {
+  static final verticalProductShadow = BoxShadow(
+    color: TColors.darkGrey.withOpacity(0.1),
+    blurRadius: 50,
+    spreadRadius: 7,
+    offset: const Offset(0, 2),
+  );
+
+  static final horizontalProductShadow = BoxShadow(
+    color: TColors.darkGrey.withOpacity(0.1),
+    blurRadius: 50,
+    spreadRadius: 7,
+    offset: const Offset(0, 2),
+  );
+}
