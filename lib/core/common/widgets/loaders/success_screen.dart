4d5d1f42eecab8c4cdab04d809ef5bd169f3841e
ommanuel <eonojefemue@bluetag.tech>
2025-09-20 19:43:17 +0100

    success screen

---
+++ lib/core/common/widgets/loaders/success_screen.dart
@@ -0,0 +1,57 @@
+import 'package:ecom/core/common/constants/text_strings.dart';
+import 'package:ecom/core/common/styles/spacing_styles.dart';
+import 'package:ecom/core/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+import '../../constants/sizes.dart';
+
+class SuccessScreen extends StatelessWidget {
+  const SuccessScreen({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subtitle,
+    required this.onPressed,
+  });
+
+  final String image, title, subtitle;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: TSpacingStyles.paddingWithAppBarHeight * 2,
+          child: Column(
+            children: [
+              Image(
+                  width: THelperFunctions.screenWidth(context) * 0.6,
+                  image: AssetImage(image)),
+              const SizedBox(height: TSizes.spaceBtwSections),
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              Text(
+                subtitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: onPressed,
+                  child: const Text(TTexts.tContinue),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
